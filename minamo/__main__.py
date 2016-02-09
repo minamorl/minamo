0d7555846ab6ac067e49d1cbb392da7cb04ec369
minamorl <minamorl@users.noreply.github.com>
2016-02-09 16:52:24 +0900

    add bigram search feature

--- minamo/__main__.py
+++ minamo/__main__.py
@@ -1,6 +1,8 @@
 from . import bot
 from . import models
+from . import ngram
 from redisorm.core import Persistent
+from redis import StrictRedis
 
 
 def show_search_results(query):
@@ -11,20 +13,34 @@ Search Result:  {keyword}
 """.format(keyword=query))
 
     p = Persistent("minamo")
+    r = StrictRedis(decode_responses=True)
 
-    for page in p.load_all(models.Page):
+    qgram = ngram.ngram(query, 2)
+    
+    resultset = None
+    for bi in list(qgram)[:-1]:
+        if resultset is None:
+            resultset = set(r.lrange("minamo:bi:{}".format(bi), 0, -1))
+        else:
+            resultset = resultset & set(r.lrange("minamo:bi:{}".format(bi), 0, -1))
+    print(resultset)
+
+    for page in (p.load(models.Page, x) for x in resultset):
         if page.title is None:
             continue
 
-        if query in page.title:
-            print("*", page.title)
-            print(" ", page.url)
+        print("*", page.title)
+        print(" ", page.url)
 
 
 if __name__ == '__main__':
     import sys
     if sys.argv[1] == "crawl":
         bot.bot(sys.argv[2])
+        sys.exit()
+
+    if sys.argv[1] == "bi":
+        ngram.create_bigram()
         sys.exit()
 
 
---
+++ minamo/ngram.py
@@ -0,0 +1,27 @@
+import redis
+from redisorm.core import Persistent
+from .models import Page, URL
+
+
+def _bi(name):
+    prefix = "minamo:bi:"
+    return prefix + name
+
+def ngram(s, n):
+    i = 0
+    while True:
+        yield s[i:i+n]
+        i += 1
+
+        if len(s) <= i:
+            return
+
+
+def create_bigram():
+    p = Persistent("minamo")
+    r = redis.StrictRedis(decode_responses=True)
+
+    for page in p.load_all(Page):
+        if page.description is not None:
+            for word in ngram(page.description, 2):
+                r.lpush(_bi(word), page.id)
